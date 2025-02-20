11145bc6d796188ec513651a3d4322c6695aae91
egodschalk <egodschalk@gmail.com>
2025-02-20 00:10:20 -0500

    contact form formatting

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,12 +1,45 @@
 import { Link, useLocation } from 'react-router-dom';
 import './Contact.css'
-import dayton1logo from '../assets/DaytOn1-Logo.png'
 
 function Contact() {
 
     return (
-        <div>
-            
+        <div className="contact-page">
+            <h2 className="contact-header">Contact Us</h2>
+            <div className='contact-content'>
+                <div className='contact-info'>
+                    <h3>Address:</h3>
+                    <p>Genuine Work</p>
+                    <p>15 McDonough Street</p>
+                    <p>Dayton, OH 45402</p>
+                    <h3>Phone (Please Text):</h3>
+                    <p>858-752-2578</p>
+                    <h3>Email:</h3>
+                    <p>DaytOn1Salsa@gmail.com</p>
+                </div>
+                <div className='contact-form'>
+                    <form className='form'>
+                        <div className='field'>
+                            <label className='label'>
+                                <input type="text" className="name" name="name" placeholder="name" />
+                            </label>
+                        </div>
+                        <div className='field'>
+                            <label className='label'>
+                                <input type="text" className="email" name="email" placeholder="email" />
+                            </label>
+                        </div>
+                        <div className='field'>
+                            <label className='label-message'>
+                                <textarea className="message" rows="8" cols="50" placeholder="your message"></textarea>
+
+                            </label>
+                        </div>
+                        <button>Contact Us</button>
+                    </form>
+                </div>
+
+            </div>
         </div>
     );
 }
