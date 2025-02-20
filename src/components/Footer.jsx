b8cc2e2008db5e5f4486b5f54c35cd4cfc1a46cb
egodschalk <egodschalk@gmail.com>
2025-02-20 01:13:54 -0500

    finished footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,25 @@
 import { Link, useLocation } from 'react-router-dom';
 import './Footer.css'
-import dayton1logo from '../assets/DaytOn1-Logo.png'
+import facebookLogo from '../assets/facebook-icon.png'
+import instagramLogo from '../assets/instagram-icon.png'
 
 function Footer() {
 
     return (
-        <div>
-            
+        <div className='footer'>
+            <div className='socials'>
+                <p className='follow'>follow us:  </p>
+                <a href="https://www.facebook.com/DaytOn1Salsa">
+                    <img src={facebookLogo} alt="" className='social-logos'/>
+                </a>
+                <a href="https://www.instagram.com/dayton1salsa/">
+                <img src={instagramLogo} alt="" className='social-logos' />
+                </a>
+            </div>
+            <div className='ownership'>
+                <p className='copyright'>&copy; 2025 DaytOn1 Salsa - All Rights Reserved</p>
+                <a href="https://www.egodschalk.com/" className='dev-stuff'>created by: ColorFlow Studios</a>
+            </div>
         </div>
     );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 import { Link, useLocation, NavLink } from 'react-router-dom';
 import './Header.css'
-import dayton1logo from '../assets/DaytOn1-Logo.png'
+import dayton1Logo from '../assets/DaytOn1-Logo.png'
 
 function Header() {
 
@@ -8,7 +8,7 @@ function Header() {
         <div className="header">
             <div className="header-logo">
                 <Link to="/">
-                    <img className="logo" src={dayton1logo} alt="dayton1 logo" />
+                    <img className="logo" src={dayton1Logo} alt="dayton1 logo" />
                 </Link>
             </div>
 
