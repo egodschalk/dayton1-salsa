84d5a93a8d07ee26b885960b4c54224a19174907
egodschalk <egodschalk@gmail.com>
2025-03-04 00:15:44 -0500

    added start of class page

--- src/pages/Classes.jsx
+++ src/pages/Classes.jsx
@@ -1,12 +1,59 @@
 import './Classes.css'
+import classesPagePic from '../assets/tim-teaching.png'
 
 export default function Classes() {
     return (
         <div className="classes-page">
             <div className='classes-content'>
-                <h2 className='classes-text'>
-                    This page is currently under construction
-                </h2>
+                <div className='classes-location'>
+                    <p>Location:</p>
+                    <p>Genuine Work</p>
+                    <p>15 Mc Donough Street</p>
+                    <p>Dayton, OH 45402</p>
+                </div>
+                <div className='classes-schedule'>
+                    <div className='days'>
+                        <h3>MONDAYS</h3>
+                        <p>Multiple Levels Offered</p>
+                    </div>
+                    <div>
+                        <h3>CLASS SCHEDULE</h3>
+                        <div className='schedule'>
+                            <div className='times'>
+                                <p>6:30</p>
+                                <p>7:15</p>
+                                <p>7:45</p>
+                            </div>
+                            <div className='classes'>
+                                <p>Bachata</p>
+                                <p>Social Dancing</p>
+                                <p>Salsa</p>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+                <div className='classes-membership-section'>
+                    <div className='classes-rates'>
+                        <h3>Monthly Membership Rates:</h3>
+                        <h3>March 2024</h3>
+                        <div className='rates'>
+                            <div className='rate-category'>
+                                <p>1 Class (Salsa OR Bachata):</p>
+                                <p>Both Classes:</p>
+                            </div>
+                            <div className='rate-cost'>
+                                <p>$45</p>
+                                <p>$60</p>
+                            </div>
+                        </div>
+                        <ul>Social dancing included in monthly memberships</ul>
+                        <ul>All monthly memberships are ONLY valid for the duration of the month that it is purchased. Classes do not roll over to the next month.</ul>
+                    </div>
+                    <div className='classes-picture'>
+                        <img src={classesPagePic} alt="" />
+                    </div>
+                </div>
             </div>
         </div>
     );
