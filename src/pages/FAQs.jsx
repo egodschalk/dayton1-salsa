c59d70ca0ee32ffacb014bcfed489458f9167cda
egodschalk <egodschalk@gmail.com>
2025-03-06 14:51:18 -0500

    added FAQ page

--- src/pages/FAQs.jsx
+++ src/pages/FAQs.jsx
@@ -1,12 +1,42 @@
 import './FAQs.css'
+import React from 'react'
+
+const faqData = [
+    {
+        id: 1,
+        question: 'How should I dress for class?',
+        answer: 'Wear comfortable clothes that you can move in. Wear comfortable clothes that you can move in. Wear comfortable clothes that you can move in. Wear comfortable clothes that you can move in',
+    },
+    {
+        id: 1,
+        question: 'How should I dress for class?',
+        answer: 'Wear comfortable clothes that you can move in',
+    },
+    {
+        id: 1,
+        question: 'How should I dress for class?',
+        answer: 'Wear comfortable clothes that you can move in',
+    },
+]
 
 export default function FAQs() {
+
+
     return (
         <div className="faqs-page">
-            <div className='faqs-content'>
-                <h2 className='faqs-text'>
-                    This page is currently under construction
+            <div className='faqs-header'>
+                <h2>
+                    Frequently Asked Questions
                 </h2>
+            </div>
+            <div className='faqs-content'>
+                {faqData.map((item, index) => (
+                    // data={proj} key={proj.id}
+                    <div key={index}>
+                    <h5>{item.question}</h5>
+                    <p>{item.answer}</p>
+                    </div>
+                ))}
             </div>
         </div>
     );
