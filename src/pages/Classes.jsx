9c44097ee10c571d73b042ea78e8c86e45e20e0a
egodschalk <egodschalk@gmail.com>
2025-05-05 17:33:38 -0400

    may monthly rates

--- src/pages/Classes.jsx
+++ src/pages/Classes.jsx
@@ -58,7 +58,7 @@ export default function Classes() {
                 <div className='classes-membership-section'>
                     <div className='classes-rates'>
                         <h3>Monthly Rates:</h3>
-                        <h3>April</h3>
+                        <h3>May</h3>
 
                         <p>We offer classes in monthly cycles</p>
                         {/* <h3>March 2024</h3> */}
